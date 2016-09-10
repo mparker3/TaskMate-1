dea1c46c10fdc24e8b69cdc2286ba349756f8fd1
Tu Tran <tu.tran@temple.edu>
2016-09-10 17:13:22 -0400

    database setup

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from app import db
+
+class Task(db.Model):
+  id = db.Column(db.String(50, primary_key=True)
+  name = db.Column(db.String(500))
+  deadline = db.Column(db.DateTime)
+  length = db.Column(db.Integer)
+  priority = db.Column(db.Integer)
+  slot = db.Column(db.Integer)
+
+class User(db.Model):
+  id = db.Column(db.String(50, primary_key=True))
+  usename = db.Column(db.String(120), index=True, unique=True)
+  email = db.Column(db.String(120), index=True, unique=True)
--- app/views.py
+++ app/views.py
@@ -1,16 +1,25 @@
 from app import app
-from flask import jsonify, abort, render_template, send_from_directory, url_for
+from flask import jsonify, \
+                  abort, \
+                  render_template, \
+                  send_from_directory, \ 
+                  url_for, request
 
 @app.route('/')
 def index():
   return render_template("index.html")
 
 @app.route('/tasks/', methods=['GET'])
+def get_all_tasks():
+  return jsonify(tasks)
+
 @app.route('/tasks/<int:date>', methods=['GET'])
 def get_sorted_tasks(date):
   tasks = ['task A', 'task B', 'task C']
   return jsonify(tasks)
 
 @app.route('/tasks', methods=['POST'])
-def add_new_task():
+def create_new_task():
+  if not request.json or not 'name' in request.json:
+    abort(400)
   return 
